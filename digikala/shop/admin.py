7e9456879ff09797c0bfd3e09cbf60b84ccba8d6
Hussein <answereeee4@gmail.com>
2023-11-13 01:28:51 +0330

    004 - creating customer model and adding it to admin

--- digikala/shop/admin.py
+++ digikala/shop/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import category_model
+from .models import category_model, customer_model
 
 # Register your models here.
 admin.site.register(category_model)
+admin.site.register(customer_model)
--- digikala/shop/models.py
+++ digikala/shop/models.py
@@ -11,3 +11,38 @@ class category_model(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+
+class customer_model(models.Model):
+    first_name=models.CharField(max_length=100, verbose_name='نام')
+    last_name=models.CharField(max_length=100, verbose_name='نام خانوادگی')
+    phone=models.CharField(max_length=20, verbose_name='شماره همراه')
+    emial=models.EmailField(max_length=100, verbose_name='آدرس ایمیل')
+    password=models.CharField(max_length=50, verbose_name='رمز عبور')
+    credit=models.IntegerField(default=0, blank=True, verbose_name='اعتبار')
+
+    class Meta:
+        verbose_name='مشتری'
+        verbose_name_plural='مشتری‌ها'
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+    
+
+
+class product_model(models.Model):
+    name=models.CharField(max_length=100, verbose_name='نام کالا')
+    slug=models.SlugField(max_length=200, verbose_name='آدرس کالا')
+    description=models.TextField(max_length=2000000, verbose_name='جزییات')
+    price=models.IntegerField(default=50000, blank=True, verbose_name='قیمت محصول')
+    category=models.ForeignKey(category_model, on_delete=models.CASCADE, verbose_name='دسته‌بندی')
+    picture=models.ImageField(upload_to='product/', verbose_name='تصویر محصول')
+
+
+    class Meta:
+        verbose_name='کالا'
+        verbose_name_plural='کالاها'
+
+    def __str__(self):
+        return self.name
