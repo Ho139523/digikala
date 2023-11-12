2d8f09c5479e637612ff33a30475d0dfbd06d051
Hussein <answereeee4@gmail.com>
2023-11-13 00:21:25 +0330

    002 - Adding media to settings.py and creating category model

--- digikala/shop/models.py
+++ digikala/shop/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class category_model(models.Model):
+    name=models.CharField(max_length=100, verbose_name='نام دسته‌بندی')
+    slug=models.SlugField(max_length=200, verbose_name='آدرس دسته‌بندی')
+
+    class Meta:
+        verbose_name='دسته‌بندی'
+        verbose_name_plural='دسته‌بندی‌ها'
+
+    def __str__(self):
+        return self.name
