6f7ab76c8fe410bf9a36bac73f2ec072b84167c8
Ho139523 <99872823+Ho139523@users.noreply.github.com>
2023-11-13 06:44:03 +0330

    005 - creating order model

--- digikala/shop/models.py
+++ digikala/shop/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
-class category_model(models.Model):
+class Category_model(models.Model):
     name=models.CharField(max_length=100, verbose_name='نام دسته‌بندی')
     slug=models.SlugField(max_length=200, verbose_name='آدرس دسته‌بندی')
 
@@ -14,13 +16,13 @@ class category_model(models.Model):
     
 
 
-class customer_model(models.Model):
+class Customer_model(models.Model):
     first_name=models.CharField(max_length=100, verbose_name='نام')
     last_name=models.CharField(max_length=100, verbose_name='نام خانوادگی')
     phone=models.CharField(max_length=20, verbose_name='شماره همراه')
     emial=models.EmailField(max_length=100, verbose_name='آدرس ایمیل')
     password=models.CharField(max_length=50, verbose_name='رمز عبور')
-    credit=models.IntegerField(default=0, blank=True, verbose_name='اعتبار')
+    credit=models.DecimalField(default=0, decimal_places=0, blank=True, max_digits=12, verbose_name='اعتبار')
 
     class Meta:
         verbose_name='مشتری'
@@ -31,11 +33,11 @@ class customer_model(models.Model):
     
 
 
-class product_model(models.Model):
+class Product_model(models.Model):
     name=models.CharField(max_length=100, verbose_name='نام کالا')
     slug=models.SlugField(max_length=200, verbose_name='آدرس کالا')
     description=models.TextField(max_length=2000000, verbose_name='جزییات')
-    price=models.IntegerField(default=50000, blank=True, verbose_name='قیمت محصول')
+    price=models.DecimalField(default=0, decimal_places=0, blank=True, max_digits=12, verbose_name='قیمت محصول')
     category=models.ForeignKey(category_model, on_delete=models.CASCADE, verbose_name='دسته‌بندی')
     picture=models.ImageField(upload_to='product/', verbose_name='تصویر محصول')
 
@@ -46,3 +48,21 @@ class product_model(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Order_model(models.Model):
+    STATUS_CHOICES=(('u', 'در دست آماده‌سازی'), ('r', 'آماده تحویل'))
+    customer=models.ForeignKey(Customer_model, on_delete=models.CASCADE, null=False, blank=False, verbose_name='مشتری')
+    product=models.ForeignKey(Product_model, on_delete=models.CASCADE, null=False, blank=False, verbose_name='کالا')
+    date=models.DateTimeField(default=timezone.now, verbose_name='تاریخ سفارش')
+    slug=models.SlugField(max_length=200, verbose_name='آدرس سفارش‌')
+    status=models.CharField(max_length=1, choices=STATUS_CHOICES, default='u', verbose_name="وضعیت سفارش")
+    quantity=models.IntegerField(max_digits=2, default=1, verbose_name='تعداد')
+    
+
+    class Meta:
+        verbose_name='سفارش'
+        verbose_name_plural='سفارشات'
+
+    def __str__(self):
+        return self.product
